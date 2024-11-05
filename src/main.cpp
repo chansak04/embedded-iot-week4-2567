a5c1c7f86f8a5faf615803c279f946b4fc6ab277
chansak04 <deawchansak@gmail.com>
2024-11-05 15:23:15 +0700

    master

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(250); //250ms = 0.25s
+  Serial.println("ESP32 Touch Test!");
+  pinMode(13,OUTPUT);   // PIN GIOP13 connect to LED
+  digitalWrite(13,LOW); // PIN GIOP13 = OFF
+}
+
+void loop()
+{
+  if (touchRead(T6)< 45)
+  {
+    Serial.println("Touch:");
+    Serial.println(touchRead(T6)); //get value using T6
+    digitalWrite(13,HIGH);         // PIN GIOP13 = ON(1)
+  }
+  else{
+    Serial.println("Touch:");
+    Serial.println(touchRead(T6)); //get value using T6
+    digitalWrite(13,LOW);          // PIN GIOP13 = OFF(0)
+  }
+    delay(1000);  // 1000ms = 1s
+}
